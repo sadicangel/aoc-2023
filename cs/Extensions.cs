67eb2cb303a51f0f1486324454a8c56583f1a1e8
sadicangel <sadicangel@hotmail.com>
2023-12-13 21:08:45 +0000

    Adds solution for day 2.

--- cs/Extensions.cs
+++ cs/Extensions.cs
@@ -3,6 +3,6 @@
 public static class Extensions
 {
     private static readonly string[] Splitters = ["\r\n", "\n"];
-    public static string[] SplitLines(this string s) => s.Split(Splitters, StringSplitOptions.TrimEntries);
-    public static string GetInputPath(this Type t, int part) => Path.GetFullPath(Path.Combine($"{t.Name}_{part}"));
+    public static string[] SplitLines(this string s) => s.Split(Splitters, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+    public static string GetInputContent(this Type t, int part) => File.ReadAllText(Path.GetFullPath(Path.Combine($"{t.Name}_{part}")));
 }
