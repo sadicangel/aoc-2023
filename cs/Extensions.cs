f5dccfadd7d0ec235f941f2f7d3ebf551e109db2
sadicangel <sadicangel@hotmail.com>
2023-12-13 20:39:30 +0000

    Adds solution for day1. (#2)

---
+++ cs/Extensions.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode2023;
+
+public static class Extensions
+{
+    private static readonly string[] Splitters = ["\r\n", "\n"];
+    public static string[] SplitLines(this string s) => s.Split(Splitters, StringSplitOptions.TrimEntries);
+    public static string GetInputPath(this Type t, int part) => Path.GetFullPath(Path.Combine($"{t.Name}_{part}"));
+}
--- cs/Program.cs
+++ cs/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdventOfCode2023;
+using System.Reflection;
+
+foreach (var type in Assembly.GetExecutingAssembly().GetTypes().Where(t => t.Name.StartsWith("Day_")))
+{
+    var part1 = type.GetMethod("Part1")?.Invoke(null, [type.GetInputPath(1)]);
+    var part2 = type.GetMethod("Part2")?.Invoke(null, [type.GetInputPath(2)]);
+    Console.WriteLine($"{type.Name} ({part1}, {part2})");
+}
